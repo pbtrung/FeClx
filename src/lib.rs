a1a979a851e6a11959889fd84487db4ff1c5172b
Trung Pham <phambaotrung@gmail.com>
2017-04-14 18:12:47 -0700

    Fix missing documentation for crate

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+//! Rust language bindings for [libsodium](https://github.com/jedisct1/libsodium)
+//!
+//! Sodium is a modern, easy-to-use software library for encryption, decryption, 
+//! signatures, password hashing and more.
+
+// For explanation of lint checks, run `rustc -W help`
+#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+          unknown_crate_types, warnings)]
+#![deny(deprecated, improper_ctypes, missing_docs,
+        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
+        private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
+        unknown_lints, unused, unused_allocation, unused_attributes, unused_comparisons,
+        unused_features, unused_parens, while_true)]
+#![warn(trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results)]
+// Allow `trivial_casts` to cast `u8` to `c_char`, which is `u8` or `i8`, depending on the
+// architecture.
+#![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
+         missing_debug_implementations, trivial_casts, unsafe_code, variant_size_differences)]
